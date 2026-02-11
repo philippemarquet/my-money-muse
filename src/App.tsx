79cb4176b7088e55a660e938c84d0cb0d7b08472
philippemarquet <129676960+philippemarquet@users.noreply.github.com>
2026-02-11 10:59:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-// src/App.tsx
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { AppLayout } from "@/components/AppLayout";
 import { AuthGate } from "@/components/AuthGate";
 
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Budgets from "./pages/Budgets";
+import BudgetDetail from "./pages/BudgetDetail";
 import Categories from "./pages/Categories";
 import Accounts from "./pages/Accounts";
 import SettingsPage from "./pages/SettingsPage";
@@ -29,9 +30,11 @@ const App = () => (
               <Route path="/" element={<Dashboard />} />
 
               <Route path="/transacties" element={<Transactions />} />
-              <Route path="/budgetten" element={<Budgets />} />
 
-              {/* list + detail via hetzelfde component (nu even snel) */}
+              <Route path="/budgetten" element={<Budgets />} />
+              {/* detail: /budgetten/2026-02-01/<categoryId> */}
+              <Route path="/budgetten/:periodStart/:categoryId" element={<BudgetDetail />} />
+
               <Route path="/categorieen" element={<Categories />} />
               <Route path="/categorieen/:id" element={<Categories />} />
 
@@ -39,6 +42,7 @@ const App = () => (
               <Route path="/rekeningen/:id" element={<Accounts />} />
 
               <Route path="/instellingen" element={<SettingsPage />} />
+
               <Route path="*" element={<NotFound />} />
             </Routes>
           </AppLayout>
